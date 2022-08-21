6c06faf4326a1f84c77130b0d00ec349b357d4ea
Dinuka Wanniarachchi <dinukaw95@gmail.com>
2022-08-21 10:21:18 +0530

    created new req.txt file

--- backend/todo_app/todo_app/settings.py
+++ backend/todo_app/todo_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r-3vlp!2g8prx%&pn-mzn1e9yc=ry28oqdd2^vi!@t_f9c0*5&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
